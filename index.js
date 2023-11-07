fc0857ba75bf725fef2f42eee63c8c34df64e06f
Joao <joaogsoares@hotmail.com>
2023-11-07 10:10:00 -0300

    Index js criado

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require ('express');
+
+const patientRouter = require('./routers/patientRouter');
+
+const app = express.json();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
+
+app.use('/patients', patientRouter)
+
+app.listen(3000, () =>
+    console.log('Aplicação de exemplo pro projeto Otto na porta 3000!')
+);
